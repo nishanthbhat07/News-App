103cf9dbb27000e0e3a6e35ee1a54869578fe67b
Nishanth Bhat <nishanth.bhat@bfhl.in>
2024-09-14 19:55:42 +0530

    configured eslint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,37 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+/* eslint-disable global-require */
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import "react-native-reanimated";
 
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { COLORS } from '@/constants/Colors';
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "../hooks/useColorScheme";
+import { COLORS } from "../constants/Colors";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.white,
+    flex: 1,
+  },
+});
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    "Satoshi-Regular": require('../assets/fonts/Satoshi-Regular.otf'),
-    "Satoshi-Medium": require('../assets/fonts/Satoshi-Medium.otf'),
-    "Satoshi-Bold": require('../assets/fonts/Satoshi-Bold.otf'),
-    "Satoshi-Italic": require('../assets/fonts/Satoshi-Italic.otf'),
+    "Satoshi-Regular": require("../assets/fonts/Satoshi-Regular.otf"),
+    "Satoshi-Medium": require("../assets/fonts/Satoshi-Medium.otf"),
+    "Satoshi-Bold": require("../assets/fonts/Satoshi-Bold.otf"),
+    "Satoshi-Italic": require("../assets/fonts/Satoshi-Italic.otf"),
   });
 
   useEffect(() => {
@@ -35,20 +46,12 @@ export default function RootLayout() {
 
   return (
     <SafeAreaView style={styles.container}>
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(news)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(news)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
     </SafeAreaView>
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: COLORS.white,
-    flex: 1,
-  },
-});
